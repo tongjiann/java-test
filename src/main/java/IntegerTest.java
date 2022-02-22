1a1f4f4b76dd1628c726cba6240a6822bc4e1fc3
xiwang <xiw@xiw.com>
2022-02-22 13:35:53 +0800

    integer test

---
+++ src/main/java/IntegerTest.java
@@ -0,0 +1,14 @@
+public class IntegerTest {
+    public static void main(String[] args) {
+        divisibleTest();
+        modTest();
+    }
+
+    private static void divisibleTest() {
+        System.out.println(8 / 2);
+    }
+
+    private static void modTest() {
+        System.out.println(9 % 2);
+    }
+}
