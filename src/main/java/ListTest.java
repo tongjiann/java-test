1bf8fbf06595c372d9541f86d5ed1b5316817f5e
xiwang <xiw@xiw.com>
2022-03-14 11:31:32 +0800

    add some test

--- src/main/java/ListTest.java
+++ src/main/java/ListTest.java
@@ -1,3 +1,6 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -5,5 +8,14 @@ public class ListTest {
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("1", "2");
         System.out.println(strings.toString());
+    }
+
+    @Test
+    public void SubListTest(){
+        List<String > list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add("1");
+        }
+        System.out.println(list.subList(1,10));
     }
 }
--- src/main/java/MapTest.java
+++ src/main/java/MapTest.java
@@ -1,12 +1,14 @@
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.junit.Test;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MapTest {
-    public static void main(String[] args) {
+    @Test
+    public  void testEncryptMap() {
         Map<String, Object> strMap = new HashMap<>();
 
         strMap.put("appKey", "DESUtil.encryption(appKey,DESUtil.DES_KEY)");
@@ -16,5 +18,11 @@ public class MapTest {
         StringEntity stringEntity = new StringEntity(json, ContentType.APPLICATION_FORM_URLENCODED);
         System.out.println(json);
         System.out.println(stringEntity);
+    }
+
+    @Test
+    public void testEmptyMap(){
+        Map<String ,String > map = new HashMap<>();
+        System.out.println(map.get("1"));
     }
 }
