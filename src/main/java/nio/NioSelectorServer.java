5c47aa92fe7a1b55efe2d8278da668bdd6c22785
xiwang <xiw@xiw.com>
2022-05-13 13:57:58 +0800

    add some test

---
+++ src/main/java/nio/NioSelectorServer.java
@@ -0,0 +1,52 @@
+package nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author xiwang
+ * @date 2022-05-07 13:52
+ */
+public class NioSelectorServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress(8765));
+        serverSocketChannel.configureBlocking(false);
+        Selector selector = Selector.open();
+        SelectionKey selectionKey = serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        System.out.println("服务器启动成功");
+        while (true) {
+            selector.select();
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while (iterator.hasNext()) {
+
+                SelectionKey next = iterator.next();
+                if (next.isAcceptable()) {
+                    ServerSocketChannel server = (ServerSocketChannel) selectionKey.channel();
+                    SocketChannel accept = server.accept();
+                    accept.configureBlocking(false);
+                    accept.register(selector,SelectionKey.OP_READ);
+                    System.out.println("客户端连接成功");
+                } else if (next.isReadable()) {
+                    SocketChannel socketChannel = (SocketChannel) next.channel();
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(128);
+                    int len = socketChannel.read(byteBuffer);
+                    if(len>0){
+                        System.out.println(new String(byteBuffer.array()));
+                    }else if(len<0){
+                        System.out.println("客户端断开连接");
+                        socketChannel.close();
+                    }
+                }
+                iterator.remove();
+            }
+        }
+
+    }
+}
