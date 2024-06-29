fcb5f0a4719fbb1d95d3fd70f6ece2c1af6b71ce
tongjiann <tongjiann@gmail.com>
2024-06-29 11:57:54 +0800

    fix: add some test

--- src/main/java/com/xiw/test/FileCopyTest.java
+++ src/main/java/com/xiw/test/FileCopyTest.java
@@ -4,14 +4,18 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileCopyTest {
 
     private static final long SIZE_THRESHOLD = 500L * 1024 * 1024; // 500MB in bytes
 
-    public static void main(String[] args) {
-        File sourceDir = new File("path/to/A");
-        File targetDir = new File("path/to/B");
+    public static void main(String[] args) throws IOException {
+//        File sourceDir = new File("/Volumes/爱国者/HHHHH/Video");
+        File sourceDir = new File("/Volumes/H");
+        File targetDir = new File("/Volumes/H");
 
         if (!sourceDir.exists() || !sourceDir.isDirectory()) {
             System.out.println("Source directory does not exist or is not a directory.");
@@ -25,18 +29,36 @@ public class FileCopyTest {
         copyLargeFiles(sourceDir, targetDir);
     }
 
-    private static void copyLargeFiles(File sourceDir, File targetDir) {
+    private static void copyLargeFiles(File sourceDir, File targetDir) throws IOException {
         File[] files = sourceDir.listFiles();
         if (files == null) return;
+        List<String> ignoreFileList = Arrays.asList(".Spotlight-V100","WATCHED",".Trashes");
 
-        for (File file : files) {
+        List<File> fileList = Arrays.stream(files).filter(e -> {
+            for (String s : ignoreFileList) {
+                if (e.getAbsolutePath().contains(s)) {
+                    return false;
+                }
+            }
+            return true;
+        }).collect(Collectors.toList());
+        for (File file : fileList) {
             if (file.isFile()) {
                 if (file.length() > SIZE_THRESHOLD) {
                     copyFile(file, new File(targetDir, file.getName()));
                 }
+                if (!file.getParentFile().toString().equals(targetDir.toPath().toString())) {
+                    System.out.println("delete file" + file.getAbsolutePath());
+                    Files.delete(file.toPath());
+                }
             } else if (file.isDirectory()) {
                 // 递归遍历子目录
                 copyLargeFiles(file, targetDir);
+                System.out.println("delete dir" + file.getAbsolutePath());
+                Files.delete(file.toPath());
+            } else {
+                System.out.println("delete unknown type file" + file.getAbsolutePath());
+                Files.delete(file.toPath());
             }
         }
     }
@@ -44,12 +66,10 @@ public class FileCopyTest {
     private static void copyFile(File sourceFile, File targetFile) {
         try {
             if (targetFile.exists()) {
-                System.out.println("Target file exists. Deleting source file: " + sourceFile.getAbsolutePath());
-                Files.delete(sourceFile.toPath());
+                System.out.println("Target file exists. : " + sourceFile.getAbsolutePath());
             } else {
-                Files.copy(sourceFile.toPath(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
                 System.out.println("Copied file: " + sourceFile.getAbsolutePath() + " to " + targetFile.getAbsolutePath());
-                Files.delete(sourceFile.toPath());
+                Files.copy(sourceFile.toPath(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
             }
         } catch (IOException e) {
             System.err.println("Error copying file: " + sourceFile.getAbsolutePath() + " to " + targetFile.getAbsolutePath());
